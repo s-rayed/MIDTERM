eece9b883598e3a747acf0e68b3e3c6b8a13fd90
Rayed <siddiqui.rayed@gmail.com>
2016-05-18 18:35:34 -0400

    added functionality to the signup button and added a signup form WOO!

--- app/actions.rb
+++ app/actions.rb
@@ -11,6 +11,25 @@ get '/sign_up' do
   erb :'/users/sign_up'
 end
 
+post '/sign_up' do
+  @user = User.new(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    address: params[:address],
+    city: params[:city],
+    province: params[:province],
+    postal_code: params[:postal_code],
+    paypal_email: params[:paypal_email],
+    password: params[:password]
+    )
+  if @user.save
+    redirect '/'
+  else
+    erb :'/users/sign_up'
+  end
+end
+
 get '/products' do 
   erb :'products/products'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
-  has_many :products, thorough: :subscriptions
+  has_many :products, through: :subscriptions
+
+  validates :first_name, :last_name, presence: true, length: {minimum: 2, maximum: 25}
+  validates :email, presence: true
+  validates :address, :city, :province, :postal_code, :paypal_email, presence: true
+  validates :password, presence: true, length: {minimum: 2}
 
 end
