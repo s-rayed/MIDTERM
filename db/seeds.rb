9da40f070a8463652d4462ebc38b0cacfc2814db
vagrant <vagrant@lighthouse-labs>
2016-05-19 19:32:40 +0000

    rake seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
-require 'faker'
+
 
 # Create main user
 user = User.create!(
+  first_name: 'jose',
+  last_name: 'vasquez',
   email: 'josevasquez@gmail.com',
+  address: '123 fake st',
+  city: 'toronto',
+  postal_code: 'm3f1b6',
+  province: 'ON',
+  paypal_email: 'josevasquez@gmail.com',
   password: 'test'
 )
 
