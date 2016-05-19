fe6a61b4eea6a4d4a1d1ae3129ce92616302058f
Rayed <siddiqui.rayed@gmail.com>
2016-05-19 14:57:44 -0400

    fixed login button demo error issue

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -49,10 +49,5 @@ $(document).ready(function() {
 
 
     $('[data-toggle="tooltip"]').tooltip();
-
-    $('button').click(function(e) {
-       e.preventDefault();
-       alert("This is a demo.\n :-)");
-    });
  });
 });
