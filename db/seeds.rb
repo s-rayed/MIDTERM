97a39e435d631af6298500a31a55f259a8f28de9
dsomel21 <dsomel21@gmail.com>
2016-05-20 11:33:18 -0400

    Added data organization, for final account over view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-
-
 # Create main user
 user = User.create!(
   first_name: 'jose',
@@ -14,7 +12,7 @@ user = User.create!(
   subscription_id: 2
 )
 
-user = User.create!(
+user2 = User.create!(
   first_name: 'sandy',
   last_name: 'Geee',
   email: 'sandy@gmail.com',
@@ -26,6 +24,7 @@ user = User.create!(
   password: 'test',
   subscription_id: 1
 )
+
 jose_product = Product.create(
   name: "Jose's Garden Pack",
   price: 8,
@@ -43,10 +42,25 @@ sandy_product = Product.create(
   user_id: 2,
   url: "http://previews.123rf.com/images/galdzer/galdzer1205/galdzer120500072/13502382-Heap-dirt-Isolated-on-white-background-Stock-Photo-dirt-mound-mud.jpg"
 )
+
+
+bb_product = Product.create(
+  name: "Weekly Bad B****es",
+  price: 100,
+  frequency: 7,
+  description: "Hoes, Condoms, Hennesey",
+  user_id: 3,
+  url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiT3dyG8-jMAhVEyj4KHUyMAQ4QjRwIBw&url=http%3A%2F%2Fwww.stereogum.com%2F562162%2Fkanye-west-the-joy-feat-pete-rock-jay-z-charlie-wilson-kid-cudi%2Fmp3s%2F&bvm=bv.122448493,d.cWw&psig=AFQjCNFKd9lHBzKFVP3fb_29RMiezkZxow&ust=1463842137057035"
+)
 subscription_jose1 = Subscription.create(
   user_id: 1,
-  product_id: 2
+  product_id: 3
 
+)
+
+subscription_jose2 = Subscription.create(
+  user_id: 1,
+  product_id: 2
 )
 ​
 subscription_sandy = Subscription.create(
