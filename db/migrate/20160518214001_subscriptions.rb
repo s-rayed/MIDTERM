c794fd42ca8cc88a93b5834313870243dab282b5
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 23:16:32 -0400

    continued debugging for heroku

--- db/migrate/20160518214001_subscriptions.rb
+++ db/migrate/20160518214001_subscriptions.rb
@@ -2,7 +2,6 @@ class Subscriptions < ActiveRecord::Migration
   
   def change
     create_table :subscriptions do |t|
-      t.string :type
       t.references :users, foreign_key: true
       t.references :product, foreign_key: true
       t.timestamps
