f6621ef08c53fdef7b6a694f525d9e52713f1d17
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 23:19:06 -0400

    continued debugging for heroku

--- db/migrate/20160521181415_add_product_id2.rb
+++
@@ -1,7 +0,0 @@
-class AddProductId2 < ActiveRecord::Migration
-  def change
-    change_table(:reviews) do |t|
-      t.references :products
-    end
-  end
-end
--- db/migrate/20160521181610_add_product_id3.rb
+++ db/migrate/20160521181610_add_product_id3.rb
@@ -1,10 +1,6 @@
 class AddProductId3 < ActiveRecord::Migration
   def change
     change_table(:reviews) do |t|
-      t.remove :products_id
-    end
-
-    change_table(:reviews) do |t|
       t.references :product
     end
   end
